0883972bacfade357c969dde5f1e22057d925f89
Steve Opperman <51415195+sopperman89@users.noreply.github.com>
2021-05-13 14:52:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import React from "react";
+import React, { Component } from "react";
 // import WebDev from "./WebDev";
 import Particles from "react-particles-js";
 import Main from "./Main";
@@ -28,25 +28,48 @@ const particlesOptions = {
   }
 };
 
-export default function App() {
-  const [currentView, setCurrentView] = React.useState("view1");
+// const [currentView, setCurrentView] = useState("view1");
 
-  let handleClick = (page) => {
-    setCurrentView(page);
-  };
+const textArray = ["a React Developer", "an Engineer", "a Problem Solver"];
 
-  return (
-    <div className="App">
-      <Particles className="particles" params={particlesOptions} />
-      {/* <WebDev /> */}
-      <Main onClick={handleClick} />
-      <div>
-        {currentView === "Main" ? (
-          <About onClick={handleClick} />
-        ) : (
-          <Projects onClick={handleClick} />
-        )}
+// let textThatChanges = textArray[this.state.textIdx % textArray.length];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { textIdx: 0 };
+  }
+
+  handleClick() {
+    console.log("click");
+  }
+
+  componentDidMount() {
+    this.timeout = setInterval(() => {
+      let currentIdx = this.state.textIdx;
+      this.setState({ textIdx: currentIdx + 1 });
+    }, 1500);
+  }
+
+  componentDidUnmount() {
+    clearInterval(this.timeout);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Main />
+        <div>
+          {/* {currentView === "Main" ? ( */}
+          <About />
+          ) : (
+          <Projects />
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default App;
