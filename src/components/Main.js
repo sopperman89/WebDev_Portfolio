276a79147391e28b1ff9b4312240eea3820eef9a
Steve Opperman <51415195+sopperman89@users.noreply.github.com>
2021-06-24 17:49:57 -0400

    Fixed CSS Fixed CSS

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import "./Main.css";
 
 const Main = ({ textThatChanges, onRouteChange }) => {
   return (
-    <section className="Main">
+    <div className="Main">
       <div className="intro flex-container">
         <h1>Hello, I am Steven Opperman</h1>
         <h3>{textThatChanges}</h3>
@@ -17,7 +17,21 @@ const Main = ({ textThatChanges, onRouteChange }) => {
           </button>
         </div>
       </div>
-    </section>
+      <div className="flex-container">
+        <div className="about">
+          <p className="white pmain">
+            I am a Front-end Web Developer fueled by determination and purpose.
+            I am ready to bring your ideas and vision to life! We will work
+            together to charge ahead and create something beautiful.
+          </p>
+        </div>
+        <div className="">
+          <p className="white pmain">
+            Lets take your project to the next level!
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/SmartBrain.js
+++ src/components/SmartBrain.js
@@ -1,21 +1,22 @@
 import React from "react";
 import img from "../images/SmartBrain.PNG";
 import "./Card.css";
-import Tilt from "react-tilt";
 
 const SmartBrain = () => {
   return (
-    <div className="Card SB">
-      <div className="image">
-        <img src={img} alt="preview" />
-      </div>
-      <div className="info">
-        <h3>SmartBrain App</h3>
-        <a href="https://www.w3schools.com" target="_blank">
-          <p>View code on Github</p>
+    <div className="Card">
+      <img src={img} alt="preview" />
+      <h3>SmartBrain App</h3>
+      <p>
+        An intuitive app that can be used to search images for faces. Built
+        using React, node.js, express.
+      </p>
+      <div className="link">
+        <a href="https://www.w3schools.com" target="_blank" rel="noreferrer">
+          Github
         </a>
-        <a href="https://www.w3schools.com" target="_blank">
-          <p>Go to the live website</p>
+        <a href="https://www.w3schools.com" target="_blank" rel="noreferrer">
+          Live
         </a>
       </div>
     </div>
