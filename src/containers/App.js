7ce3420318dfcc6a9298ea318d22e3e5ac54a8dc
Steve Opperman <51415195+sopperman89@users.noreply.github.com>
2021-06-25 12:00:37 -0400

    Updated particles. Updated particles.

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,21 +9,51 @@ const textArray = ["React Developer", "Engineer", "Problem Solver"];
 
 const particlesOptions = {
   particles: {
+    number: {
+      value: 110,
+      density: {
+        enable: true,
+        value_area: 600
+      }
+    },
     color: {
       value: "#000000"
     },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 6,
+        color: "#000000"
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 0.3,
+      random: true
+    },
     links: {
       color: "#000000",
-      distance: 150,
+      distance: 110,
       enable: true,
-      opacity: 0.5,
-      width: 1
+      opacity: 0.3,
+      width: 3
     },
-    number: {
-      value: 100,
-      density: {
-        enable: true,
-        value_area: 800
+    move: {
+      enable: true,
+      speed: 3,
+      direction: "bottom-right",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
       }
     }
   }
