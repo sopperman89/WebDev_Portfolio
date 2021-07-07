9710db72e9b8f8065f493b20814517f693ecc632
Steve Opperman <51415195+sopperman89@users.noreply.github.com>
2021-07-07 15:24:50 -0400

    Adding Styled Components Adding Styled Components

---
+++ src/components/Card.js
@@ -0,0 +1,43 @@
+import React from "react";
+// import "./Card.css";
+import img from "../images/SmartBrain.PNG";
+import styled from "styled-components";
+
+const Style = styled.div`
+  display: inline-block;
+  padding: 4px 8px;
+  font-size: 0.815em;
+  font-weight: 700;
+  line-height: 1.25;
+  letter-spacing: 0.025em;
+  color: #333333;
+  background: #ffffff;
+  border: 2px solid #ffffff;
+  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.04);
+  border-radius: 4px;
+  user-select: none;
+  cursor: default;
+
+  ${(props) =>
+    props.fixed &&
+    `
+    z-index: 10000;
+    position: fixed;
+    top: 12px;
+    left: 12px;
+  `};
+`;
+
+const Card = ({ name, email, id }) => {
+  return (
+    <Style>
+      <img src={img} alt="preview" />
+      <div>
+        <h2>Project Name</h2>
+        <p>Links</p>
+      </div>
+    </Style>
+  );
+};
+
+export default Card;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,58 @@
 import React from "react";
-import SmartBrain from "./SmartBrain";
-import ToDo from "./ToDo";
-import MacCal from "./MacCal";
+import Card from "./Card";
+// import ToDo from "./ToDo";
+// import MacCal from "./MacCal";
 import "./Projects.css";
+import styled from "styled-components";
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  min-height: -webkit-fill-available; /* mobile viewport bug fix */
+  overflow-x: auto;
+  scroll-behavior: smooth;
+`;
+
+const Grid = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  padding-left: calc(50vw - 160px);
+
+  /* Fake padding-right */
+  &::after {
+    content: "";
+    position: relative;
+    display: block;
+    flex-shrink: 0;
+    width: calc(50vw - 160px);
+    height: 1px;
+  }
+
+  > button {
+    margin-right: 40px;
+  }
+
+  /* Hide the others cards */
+  > button:not(:first-child) {
+    visibility: visible; /* switch to 'visible' */
+  }
+`;
 
 const Projects = () => {
   return (
-    <div className="proj flex-container">
-      <div className="cardCon">
-        <SmartBrain />
-        <SmartBrain />
-        <SmartBrain />
-        <SmartBrain />
-        <SmartBrain />
-      </div>
-    </div>
+    <Page>
+      <Grid>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Grid>
+    </Page>
   );
 };
 
